40ba6037fa51a2333553b0e64570710b30dc0040
lincenying <lincenying@qq.com>
2017-10-19 23:19:58 +0800

    关闭 SourceMap

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '/dist/',
-        productionSourceMap: true,
+        productionSourceMap: false,
 
         // Gzip off by default as many popular static hosts such as
         // Surge or Netlify already gzip all static assets for you.
