029cae58fdf7af44c5d5c40cbbd9d7d8159d01a6
林岑影 <lincenying@qq.com>
2018-04-25 13:50:27 +0800

    up vue-loader

--- build/webpack.server.conf.js
+++ build/webpack.server.conf.js
@@ -37,7 +37,7 @@ module.exports = merge(base, {
     // https://github.com/liady/webpack-node-externals
     externals: nodeExternals({
         // do not externalize CSS files in case we need to import it from a dep
-        whitelist: [/\.(css|vue)$/]
+        whitelist: [/\.css$/, /\?vue&type=style/]
     }),
     plugins: [
         new webpack.DefinePlugin({
