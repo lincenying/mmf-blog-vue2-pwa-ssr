860b6bd1c85f95a49d2a707422f6042eee26a5f8
林岑影 <lincenying@qq.com>
2019-06-06 18:12:17 +0800

    up

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ require('./server/models/article')
 require('./server/models/category')
 require('./server/models/comment')
 require('./server/models/user')
+require('./server/models/shihua')
 // 引入 api 路由
 const routes = require('./server/routes/index')
 
