7db1a5c9b92e2c375811264c14f86500e11828e2
林岑影 <lincenying@qq.com>
2021-04-20 15:17:56 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -44,6 +44,17 @@ module.exports = {
                                 statuses: [0, 200]
                             }
                         }
+                    },
+                    {
+                        urlPattern: /^https:\/\/fdn\.geekzu\.org/,
+                        handler: 'NetworkFirst',
+                        options: {
+                            networkTimeoutSeconds: 1,
+                            cacheName: 'avatar-cache',
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
                     }
                 ]
             })
